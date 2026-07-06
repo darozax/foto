f73ee28fb31a3f6b3fc780b350c2f3c1aaf151cc
darozax <darozax@vp.pl>
2026-07-07 01:00:24 +0200

    Fetch gallery data from JSON and display images

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,32 @@
-[
-  {
-    "file":"001.jpg",
-    "title":"Pierwsze zdjęcie"
-  },
-  {
-    "file":"002.jpg",
-    "title":"Drugie zdjęcie"
-  }
-]
+fetch("gallery.json")
+
+.then(response=>response.json())
+
+.then(data=>{
+
+const gallery=document.getElementById("gallery");
+
+data.forEach(photo=>{
+
+const div=document.createElement("div");
+
+div.className="photo";
+
+div.innerHTML=`
+
+<img
+loading="lazy"
+src="images/thumbs/${photo.file}"
+data-full="images/full/${photo.file}"
+alt="${photo.title}"
+>
+
+<p>${photo.title}</p>
+
+`;
+
+gallery.appendChild(div);
+
+});
+
+});
