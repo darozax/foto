e407da72dc2095d7f6b7d7cf293d7d097d62abaf
darozax <darozax@vp.pl>
2026-07-07 02:27:58 +0200

    Implement lightbox functionality for images

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,1 +1,24 @@
+const lightbox =
+document.getElementById("lightbox");
 
+
+const image =
+document.getElementById("lightbox-img");
+
+
+
+function openLightbox(src){
+
+image.src=src;
+
+lightbox.style.display="flex";
+
+}
+
+
+
+lightbox.onclick=function(){
+
+lightbox.style.display="none";
+
+}
